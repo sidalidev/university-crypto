1ad8993603fd5fbe94183a763fc483683442e89a
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-04-18 14:50:51 +0200

    Encrypt with an int key

--- caesar.py
+++ caesar.py
@@ -1,1 +1,16 @@
-print('Caesar Cipher')
+alphabet = 'ABCDEFGHIJKLMNOPKRSTUVWXYZ'
+message = 'HELLO'
+key = 1
+
+message_length = len(message)
+
+encrypted_message = ''
+
+for i in range(message_length):
+    caracter = message[i]
+    location = alphabet.find(caracter)
+    new_location = (location + key) % 26
+    encrypted_message += alphabet[new_location]
+    pass
+
+print(encrypted_message)
