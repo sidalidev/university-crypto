d1813094f4ca736d2ca0f5012d124a1615b79325
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-04-18 17:06:00 +0200

    handling caracter cases

--- caesar.py
+++ caesar.py
@@ -1,26 +1,36 @@
-alphabet = 'ABCDEFGHIJKLMNOPKRSTUVWXYZ'
+alphabet = 'abcdefghijklmnopkrstuvwxyz'
 
 
 def encrypt(message, key):
     encrypted_message = ''
+    key_index = alphabet.find(key)
     for i in range(len(message)):
         caracter = message[i]
-        location = alphabet.find(caracter)
+        location = alphabet.find(key_index)
         new_location = (location + key) % 26
         encrypted_message += alphabet[new_location]
     pass
-    return encrypted_message
+    return encrypted_message.upper()
 
 
 def decrypt(encrypted_message, key):
     decrypted_message = ''
+    key_index = alphabet.find(key)
     for i in range(len(encrypted_message)):
         caracter = encrypted_message[i]
         location = alphabet.find(caracter)
-        new_location = (location - key) % 26
+        new_location = (location - key_index) % 26
         decrypted_message += alphabet[new_location]
     pass
-    return decrypted_message
+    return decrypted_message.upper()
 
 
-print(decrypt('IFMMP', 1))
+# print(decrypt('MZMVCVDFULCVUVTIPGKF', 'R'))
+
+message = input('Entrez votre message: ')
+key = input('Entrez votre cle: ')
+
+message = message.lower()
+key = key.lower()
+
+print(decrypt(message, key))
